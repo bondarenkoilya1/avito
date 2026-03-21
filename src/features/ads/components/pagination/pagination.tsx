dbaa9227edd1125ab5252c3dee73ba6d9e6ae33f
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2026-03-21 19:36:36 +0300

    refactor: set fixed pagesize for pagination

--- src/features/ads/components/pagination/pagination.tsx
+++ src/features/ads/components/pagination/pagination.tsx
@@ -2,5 +2,5 @@ import { Pagination } from "antd";
 import type { JSX } from "react";
 
 export const PaginationComponent = (): JSX.Element => {
-  return <Pagination total={50} />;
+  return <Pagination total={50} pageSize={10} showSizeChanger={false} />;
 };
