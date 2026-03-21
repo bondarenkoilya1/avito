ad1ba5587c429625fc00b0c6012cbd19bf6b0374
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2026-03-21 19:33:24 +0300

    feat: setup pagination ui

--- src/features/ads/pages/ads-list-page.tsx
+++ src/features/ads/pages/ads-list-page.tsx
@@ -1,12 +1,15 @@
 import type { JSX } from "react";
 
-import { Filter } from "@/features/ads/components";
+import { Filter, PaginationComponent } from "@/features/ads/components";
 
 export const AdsListPage = (): JSX.Element => {
   return (
     <>
       <h1>Ads list</h1>
       <Filter />
+      <div style={{ position: "absolute", bottom: "10%" }}>
+        <PaginationComponent />
+      </div>
     </>
   );
 };
