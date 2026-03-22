07c0635d08e9e711bb2bfdc9915b6f54cac47c00
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2026-03-22 11:16:53 +0300

    feat: create list page header and pluralize function

--- src/features/ads/pages/ads-list-page.tsx
+++ src/features/ads/pages/ads-list-page.tsx
@@ -1,12 +1,28 @@
+import { Typography } from "antd";
 import type { JSX } from "react";
 
 import { Filter, PaginationComponent } from "@/features/ads/components";
 
+import { pluralize, type PluralizeVariants } from "@/shared/lib";
+
+const ADS_PLURAL_VARIANTS: PluralizeVariants = {
+  one: "объявление",
+  few: "объявления",
+  many: "объявлений",
+  other: "объявления"
+};
+
+const formatAdsNumber = (variants: PluralizeVariants, number: number): string =>
+  number ? `${number} ${pluralize(variants, number)}` : "Объявлений не найдено";
+
 export const AdsListPage = (): JSX.Element => {
   return (
     <>
-      <h1>Ads list</h1>
-      <Filter />
+      <Typography.Title level={3}>Мои объявления</Typography.Title>
+      <Typography.Text>{formatAdsNumber(ADS_PLURAL_VARIANTS, 42)}</Typography.Text>
+      <aside>
+        <Filter />
+      </aside>
       <div style={{ position: "absolute", bottom: "10%" }}>
         <PaginationComponent />
       </div>
