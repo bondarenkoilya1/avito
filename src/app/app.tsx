0e24d1ee0548f4fba1575f79b9496bfd33657c61
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2026-03-22 20:16:17 +0300

    fix: remove server submodule, add as directory

--- src/app/app.tsx
+++
@@ -1,6 +0,0 @@
-import type { JSX } from "react";
-import { Outlet } from "react-router-dom";
-
-export const App = (): JSX.Element => {
-  return <Outlet />;
-};
--- src/features/ads/pages/ad-details-page.tsx
+++
@@ -1,8 +0,0 @@
-import type { JSX } from "react";
-import { useParams } from "react-router-dom";
-
-export const AdDetailsPage = (): JSX.Element => {
-  const { id } = useParams();
-
-  return <h1>Ad details: {id}</h1>;
-};
