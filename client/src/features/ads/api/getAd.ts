dd047d41828d30475c624c2557f658faac79f813
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2026-03-24 19:44:26 +0300

    feat: connect ollama, write own axios instance for ai api, separate instances by feature

--- client/src/features/ads/api/getAd.ts
+++ client/src/features/ads/api/getAd.ts
@@ -1,6 +1,5 @@
+import { api } from "@/features/ads/api";
 import type { AdType } from "@/features/ads/types/ad.types";
-
-import { api } from "@/shared/api";
 
 export type AdResponse = AdType & { needsRevision: boolean };
 
