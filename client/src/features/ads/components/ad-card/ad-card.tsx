2ff24704c1ac546644396edca16c880405fee9af
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2026-03-24 16:45:08 +0300

    feat: make ad card clickable, navigate to detailed view page, component for gallery list

--- client/src/features/ads/components/ad-card/ad-card.tsx
+++ client/src/features/ads/components/ad-card/ad-card.tsx
@@ -1,5 +1,6 @@
 import { Card, Flex, Typography } from "antd";
 import type { JSX } from "react";
+import { Link } from "react-router-dom";
 
 import { AdCardCover, AdCardRevisionTag } from "@/features/ads/components";
 import { formatPrice } from "@/features/ads/components/ad-card/ad-card.utils";
@@ -10,15 +11,17 @@ import css from "./ad-card.module.css";
 type AdCardProps = { ad: AdCardType };
 
 export const AdCard = ({ ad }: AdCardProps): JSX.Element => {
-  const { title, price, category, needsRevision } = ad;
+  const { id, title, price, category, needsRevision } = ad;
 
   return (
-    <Card hoverable className={css.card} cover={<AdCardCover category={category} />}>
-      <Flex vertical gap={4} className={css.flexWrapper}>
-        <Typography.Text strong>{title}</Typography.Text>
-        <Typography.Text strong>{formatPrice(price)}</Typography.Text>
-        {needsRevision && <AdCardRevisionTag />}
-      </Flex>
-    </Card>
+    <Link to={`/ads/${id}`}>
+      <Card hoverable className={css.card} cover={<AdCardCover category={category} />}>
+        <Flex vertical gap={4} className={css.flexWrapper}>
+          <Typography.Text strong>{title}</Typography.Text>
+          <Typography.Text strong>{formatPrice(price)}</Typography.Text>
+          {needsRevision && <AdCardRevisionTag />}
+        </Flex>
+      </Card>
+    </Link>
   );
 };
---
+++ client/src/features/ads/components/ad-details-gallery/ad-details-gallery.tsx
@@ -0,0 +1,18 @@
+import { Flex } from "antd";
+import type { JSX } from "react";
+
+import { PlaceholderGallery } from "@/features/ads/components/placeholder-gallery";
+
+import css from "./ad-details-gallery.module.css";
+
+type AdDetailsGalleryProps = {
+  photos?: string[];
+};
+
+export const AdDetailsGallery = ({ photos }: AdDetailsGalleryProps): JSX.Element => {
+  return (
+    <Flex vertical gap={12} className={css.gallery}>
+      <PlaceholderGallery photos={photos} />
+    </Flex>
+  );
+};
