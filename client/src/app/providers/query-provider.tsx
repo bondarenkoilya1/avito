905c1a0e3f405977a0ab0c7b9a925ae9ae6ca413
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2026-03-25 18:00:52 +0300

    feat: add state time for queries

--- client/src/app/providers/query-provider.tsx
+++ client/src/app/providers/query-provider.tsx
@@ -4,7 +4,13 @@ import type { JSX } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5
+    }
+  }
+});
 
 export const QueryProvider = ({ children }: { children: ReactNode }): JSX.Element => {
   return (
